d91f7ae4feff68e2d3ae049d151456b817d066e4
YGOR HOLOWKA <68624664+Ygor-Holowka@users.noreply.github.com>
2024-10-11 10:24:15 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+function showSection(sectionId) {
+    
+    const sections = document.querySelectorAll('.section');
+    sections.forEach(section => {
+      section.style.display = 'none';
+    });
+  
+  
+    const activeSection = document.getElementById(sectionId);
+    activeSection.style.display = 'block';
+  }
+  
+  function toggleDetails(id) {
+    var details = document.getElementById(id);
+    if (details.style.display === "none") {
+        details.style.display = "block";
+    } else {
+        details.style.display = "none";
+    }
+}
+
+
+
+  
+  // Exibir a seção "Sobre Mim" por padrão ao carregar a página
+  
